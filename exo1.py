b47a86485e2db8f3cec54052c70728495f0d6bcb
Mohamed Berrag <berragmohamed10@gmail.com>
2025-12-08 20:57:21 +0100

    Refactor User class and clean up comments

--- exo1.py
+++ exo1.py
@@ -1,26 +1,22 @@
-#Violations in the original class
-
-#model 
-class User:
+ class User:
     def __init__(self, name, email):
         self.name = name
         self.email = email
-#user management
 class UserRepository:
     def __init__(self):
         self.users = []
 
     def add_user(self, user):
         self.users.append(user)
-#DATAbE 
+ 
 class DatabaseService:
     def save(self, user):
         print(f"Saving {user.name} to database...")
-#EMAIL
+
 class EmailService:
     def send_welcome_email(self, user):
         print(f"Sending welcome email to {user.email}")
-#REPORTING
+
 class ReportService:
     def generate(self, users):
         print("Generating report for users:", [u.name for u in users])
