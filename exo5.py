1bca7d940ae2d589491e5c53f52c559d666f426e
Mohamed Berrag <berragmohamed10@gmail.com>
2025-12-14 10:46:30 +0100

    Remove print statements from notification services
    Removed print statements from EmailService, SMSService, and PushService classes.

--- exo5.py
+++ exo5.py
@@ -7,20 +7,12 @@ class NotificationService(ABC):
 
 class EmailService(NotificationService):
     def send(self, user, message):
-        print(f"Email to {user}: {message}")
+       
 
 class SMSService(NotificationService):
     def send(self, user, message):
-        print(f"SMS to {user}: {message}")
+        
 
 class PushService(NotificationService):
     def send(self, user, message):
-        print(f"Push notification to {user}: {message}")
-
-class UserNotification:
-    def __init__(self, notifier: NotificationService):
-        self.notifier = notifier
-
-    def notify(self, user, message):
-        self.notifier.send(user, message)
-
+       
